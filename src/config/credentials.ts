1e4a1b718a8224a5bc668f236fb35e66ea5bf4d1
hardikvyazz <hardikvyas.dev@gmail.com>
2025-01-28 19:44:00 +0530

    folder stucture updated

---
+++ src/config/credentials.ts
@@ -0,0 +1,7 @@
+export const EMAIL_CONFIG = {
+    user: process.env.EMAIL || "fbl@kensciomail.com", // Gmail address
+    appPassword: process.env.APP_PASSWORD || "your-app-password", // App password
+    imapHost: "imap.gmail.com", // IMAP host for Gmail
+    imapPort: 993, // IMAP port for Gmail
+  };
+  
