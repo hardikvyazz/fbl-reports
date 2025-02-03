3f878af0aead2c6c4e6580fb38bd944bb8ec5f15
hardikvyazz <hardikvyas.dev@gmail.com>
2025-02-03 14:59:33 +0530

    cred added

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,6 +1,6 @@
 export const EMAIL_CONFIG = {
     user: process.env.EMAIL || "fbl@kensciomail.com", // Gmail address
-    appPassword: process.env.APP_PASSWORD || "your-app-password", // App password
+    appPassword: process.env.APP_PASSWORD || "hkfj bdoy whue cdjq", // App password
     imapHost: "imap.gmail.com", // IMAP host for Gmail
     imapPort: 993, // IMAP port for Gmail
   };
