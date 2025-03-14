5077bb6ef36a9a1699060a8c029187572304b158
hardikvyazz <hardikvyas.dev@gmail.com>
2025-03-14 15:24:50 +0530

    credentials.ts added to .gitignore and tracking stoped

--- src/config/credentials.ts
+++
@@ -1,7 +0,0 @@
-export const EMAIL_CONFIG = {
-    user: process.env.EMAIL || "fbl@kensciomail.com", // Gmail address
-    appPassword: process.env.APP_PASSWORD || "", // App password
-    imapHost: "imap.gmail.com", // IMAP host for Gmail
-    imapPort: 993, // IMAP port for Gmail
-  };
-  
